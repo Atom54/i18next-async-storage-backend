bc6e2f6fc20a68c06b9310010ec9e23b9a495f0a
Atom54 <38189536+Atom54@users.noreply.github.com>
2021-06-18 11:15:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as utils from './utils';
 
 // get from whatever version of react native that is being used.
